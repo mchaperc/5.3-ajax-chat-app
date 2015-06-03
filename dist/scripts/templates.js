727027984511a7df3b210b340828f14b98bfa493
Matt Chastain <matthewdchastain@gmail.com>
2015-06-03 11:38:29 -0400

    Basic templating and routing for login and chat

---
+++ dist/scripts/templates.js
@@ -0,0 +1,10 @@
+this["JST"] = this["JST"] || {};
+this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "";
+},"useData":true});
+this["JST"]["chat"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "howdy";
+},"useData":true});
+this["JST"]["login"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    return "<form action=\"\" class=\"login-form\">\n  <input class=\"login-form-username\" type=\"text\" placeholder=\"Username\" required autofocus>\n  <input type=\"submit\" value=\"Log In\">\n</form>";
+},"useData":true});
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,44 @@
 (function(){
   'use strict';
 
+  var username = '';
+
   $(document).ready(function(){
     $('body').prepend(JST['application']());
+
+    route();
+
+    $(document).on('submit', '.login-form', function(e) {
+	  	e.preventDefault();
+	  	window.location.hash = '/chat';
+	  	username = ($(this).find('.login-form-username').val());
+	})
+
+	$(window).on('hashchange', function(e) {
+		route();
+	})
+
   });
+
+  function route() {
+  	switch(window.location.hash) {
+    	case '': 
+    		renderLogin();    		
+    		break;
+    	case '#/chat':
+    		renderChat();
+    		break;  		
+    }
+  }
+
+  function renderLogin() {
+  	$('.application').html(JST['login']());
+  	console.log('login');
+  }
+
+  function renderChat() {
+	$('.application').html(JST['chat']());  	
+	console.log(username);
+  }
+
 })();
